1110306d99e389560d2c8284e99ffac51b11196a
maijjoo <maijjoo@naver.com>
2023-03-03 00:45:59 +0900

    기초 컨트롤러 및 컨트롤러 테스트

---
+++ src/main/java/com/crudpractice/Application.java
@@ -0,0 +1,11 @@
+package com.crudpractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication                  //스프링부트 자동 설정 어노테이션
+public class Application {              //프로젝트 메인클래스
+    public static void main(String[] args){
+        SpringApplication.run(Application.class, args);     //내장 WAS 실행
+    }
+}
