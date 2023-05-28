094bbcdc8baf9239493e87fc7295d220d900a984
maijjoo <maijjoo@naver.com>
2023-05-28 21:53:17 +0900

    게시글 목록 화면 구현 글 생성, 수정, 삭제 화면 및 기능 구현

--- src/main/java/com/crudpractice/sevice/posts/PostsService.java
+++ src/main/java/com/crudpractice/sevice/posts/PostsService.java
@@ -2,12 +2,16 @@ package com.crudpractice.sevice.posts;
 
 import com.crudpractice.domain.posts.Posts;
 import com.crudpractice.domain.posts.PostsRepository;
+import com.crudpractice.web.dto.PostsListResponseDto;
 import com.crudpractice.web.dto.PostsResponseDto;
 import com.crudpractice.web.dto.PostsSaveRequestDto;
 import com.crudpractice.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -27,9 +31,23 @@ public class PostsService {
         return id;
     }
 
+    @Transactional
+    public void delete(Long id){
+        Posts posts = postsRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id = "+ id));
+        postsRepository.delete(posts);
+    }
+
     public PostsResponseDto findById(Long id){
         Posts entity = postsRepository.findById(id)
                 .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id = "+ id));
         return new PostsResponseDto(entity);
+    }
+
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc(){
+        return postsRepository.findAllDesc().stream()
+                .map(PostsListResponseDto::new)
+                .collect(Collectors.toList());
     }
 }
